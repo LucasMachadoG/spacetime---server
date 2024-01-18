56d89eb62e419575e871eeb343d5e4fba00ffecb
LucasMachadoG <lucasgaelzermachado@gmail.com>
2024-01-18 13:18:10 -0300

    upload routes

--- src/server.ts
+++ src/server.ts
@@ -5,12 +5,21 @@ import cors from "@fastify/cors"
 import jwt from '@fastify/jwt';
 import { memoriesRoutes } from "./routes/memories";
 import { authRoutes } from './routes/auth';
+import multipart from '@fastify/multipart'
+import { uploadRoutes } from './routes/upload';
+import { resolve } from 'node:path';
 
 const app = fastify()
 
 //Esse metodo do fastify serve para registrar um arquivo de rotas separado
 app.register(memoriesRoutes)
 app.register(authRoutes)
+app.register(uploadRoutes)
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../upload'),
+  prefix: '/upload'
+})
+app.register(multipart)
 app.register(cors, { 
   origin: true, //Todas as URLs de front-end podem acessar nosso back-end
 })
